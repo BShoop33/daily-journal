5a79abf75f05d40c94dc0bd4e57e47d5ce59095d
bshoop <braxton.shoop@gmail.com>
2020-09-26 10:23:00 -0500

    working on deletion function v1

---
+++ scripts/JournalList.js
@@ -0,0 +1,34 @@
+import { getEntry, useEntry, deleteEntry } from './JournalDataProvider.js'
+
+const eventHub = document.querySelector(".container")
+
+const render = (journalEntryArray) => {
+    const contentTarget = document.querySelector(".entriesContainer")
+    contentTarget.innerHTML = `
+    ${journalEntryArray.map(entryObj => {
+        return `
+            <section class="journalEntry"></section>
+            <ul id="newJournalEntry">
+            <li id="newDate"><h2>${entryObj.dateEntry}</h2></li>
+            <li id="newConcepts"><p><strong>Concepts Covered</strong>:  ${entryObj.conceptEntry}</p></li>
+            <li id="newEntry"><p><strong>Journal Entry</strong>:  ${entryObj.journalEntry}</p></li>
+            <li id="newMood"><p><strong>Mood</strong>:  ${entryObj.moodEntry}</p></li>
+            <button onClick="location.reload(true)" id="deleteEntry--${entryObj.id}">Delete</button>
+            </ul>
+        `
+    }).join("")}
+    `
+};
+
+export const entryList = () => {
+    getEntry()
+        .then(render)
+};
+
+eventHub.addEventListener("click", event => {
+    event.preventDefault()
+    if (event.target.id.startsWith("deleteEntry--")) {
+        const [prefix, id] = event.target.id.split("--")
+        deleteEntry(id)
+    }
+});
